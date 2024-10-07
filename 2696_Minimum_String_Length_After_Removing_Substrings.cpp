3236f0be232a18142884a612df028e5265faf8d1
freakin23 <rameezparwez@gmail.com>
2024-10-07 10:57:09 +0530

    update 2696

---
+++ 2696_Minimum_String_Length_After_Removing_Substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minLength(string s) {
+        bool ok = true;
+        while (ok) {
+            ok = false;
+            for (int i = 0; i + 1 < (int)s.size(); i++) {
+                if (s[i] == 'A' and s[i + 1] == 'B') {
+                    ok = true;
+                    s.erase(s.begin() + i);
+                    s.erase(s.begin() + i);
+                }
+                else if (s[i] == 'C' and s[i + 1] == 'D') {
+                    ok = true;
+                    s.erase(s.begin() + i);
+                    s.erase(s.begin() + i);
+                }
+            }
+        }
+        
+        return (int)s.size();
+    }
+};
