8349f04f78a01fc417b91fcb4e3497d0ff0bbeb2
freakin23 <rameezparwez@gmail.com>
2024-10-08 12:34:28 +0530

    update 1963_LC

---
+++ 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int res = 0;
+        int open = 0;
+        for (int i = 0; i < (int)s.size(); i++) {
+            if (s[i] == ']') {
+                if (open) {
+                    open--;
+                } else {
+                    res += 1;
+                    open++;
+                }
+            } else {
+                open++;
+            }
+        }   
+        return res;
+    }
+};
