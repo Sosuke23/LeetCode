17187bb93cee10a08ee3f90c5974e9e92e1b5053
freakin23 <rameezparwez@gmail.com>
2024-10-09 11:12:25 +0530

    update LC 921

---
+++ 921_Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int res = 0;
+        int open = 0;
+        for (int i = 0; i < (int)s.size(); i++) {
+            if (s[i] == '(') {
+                open++;
+            } else {
+                if (open) {
+                    open--;
+                } else {
+                    res++;
+                }
+            }
+        }
+        return res + open;
+    }
+};
