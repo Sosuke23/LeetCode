72078aedfcdbd7864224125c51149fad594bc57e
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-10-17 12:23:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (91.75%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        std::vector<int> p;
-        int res = 0;
-        for (auto x : nums) {
-            if (x != val) {
-                res += 1;
-                p.push_back(x);
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
-        nums = p;
-        return res;
+        return index;
     }
 };
