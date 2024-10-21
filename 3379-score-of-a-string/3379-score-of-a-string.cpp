4175d02d34b429895f8b49543fdf7ed9a2f1b432
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-10-21 13:44:35 +0530

    Time: 3 ms (29.2%), Space: 8.4 MB (73.92%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int scoreOfString(string s) {
         int res = 0;
-        for (int i = 1; i < (int)s.size(); i++) {
-            res += abs((int)s[i] - (int)s[i - 1]);
+        for (int i = 0; i + 1 < (int)s.size(); i++) {
+            res += abs((s[i] - 'a') - (s[i + 1] - 'a'));
         }
         return res;
-    }  
+    }
 };
