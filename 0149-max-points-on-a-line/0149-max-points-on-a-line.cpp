86670cf881c803a5e390054b73a31af0f2353eef
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-10-26 12:54:45 +0530

    Time: 15 ms (96.34%), Space: 16.3 MB (68.52%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    unordered_map<float, int> umap;
     int maxPoints(vector<vector<int>>& points) {
         int n = (int)points.size();
         int res = 0;
         for (int i = 0; i < n; i++) {
+	    	unordered_map<float, int> umap;
             for (int j = i + 1; j < n; j++) {
-                float key = 0.0;
+                float slope = 0.0;
                 if (points[i][0] - points[j][0] == 0) { // avoid division by 0
-                    key = INT_MAX;
+                    slope = INT_MAX;
                 }
                 else {
-                    key = float(points[i][1] - points[j][1]) /
+                    slope = float(points[i][1] - points[j][1]) /
                           float(points[i][0] - points[j][0]);
                 }
 
-                umap[key]++;
+                umap[slope]++;
             }
-            for (auto &[_, key] : umap) {
-                res = max(res, key);
+            for (auto &[_, slope] : umap) {
+                res = max(res, slope);
             }
-            umap.clear();
         }
         return res + 1;
     }
