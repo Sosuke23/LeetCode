d8998ce5ed9993543b582c842c1adb6275c1614f
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-10-30 19:53:10 +0530

    Time: 3 ms (47.52%), Space: 30.3 MB (32.61%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        int p[101] = {0};
+        int p[102] = {0};
         int n = (int)nums.size();
         for (vector<int> v : nums) {
-            for (int i = v[0]; i <= v[1]; i++) {
-                p[i] = 1;
+            p[v[0]] += 1;
+            p[v[1] + 1] -= 1;
+        }
+        int res = 0;
+        int cnt = 0;
+        for (int i = 1; i <= 101; i++) {
+            cnt += p[i];
+            if (cnt > 0) {
+                res += 1;
             }
         }
-        int res = count(p + 1, p + 101, 1);
         return res;
     }
 };
