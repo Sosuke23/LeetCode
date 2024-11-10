e537e787127f472c376de75556aa30afb90e8e5c
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-11-10 12:55:54 +0530

    Time: 66 ms (54.95%), Space: 88.5 MB (31.48%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int n = (int)nums.size();
-        vector<int> pref(n + 1), suff(n + 1);
-        int res = n + 1;
-        if (*max_element(begin(nums), end(nums)) >= k) {
+        if (k == 0) {
             return 1;
         }
+        int n = (int)nums.size();
+        int res = n + 1, j = 0, kk = 0;
+        array<int, 32> C {0};
+        auto Add = [&] (int idx) {
+            for (int i = 0; i < 31; i++) {
+                if ((1 << i) & nums[idx]) {
+                    C[i]++;
+                    if (C[i] == 1) {
+                        kk += (1 << i);
+                    }
+                }
+            }
+        };
+
+        auto Remove = [&] (int idx) {
+            for (int i = 0; i < 31; i++) {
+                if ((1 << i) & nums[idx]) {
+                    C[i]--;
+                    if (C[i] == 0) {
+                        kk -= (1 << i);
+                    }
+                }
+            }
+        };
+
         for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] | nums[i];
-        }
-        
-        int j = 0, l = 0;
-        for (int i = 1; i <= n; i++) {
-            int a = pref[i];
-            if (a >= k) {
-                res = min(res, i);
-                while (j <= i and a >= k) {
-                    a ^= pref[j];
-                    res = min(res, i - j + 1);
-                    j++;
-                }
+            while (j < n and kk < k) {
+                Add(j);
+                ++j;
             }
-            int b = pref[n] ^ pref[i - 1];
-            l = i;
-            if (b >= k) {
-                res = min(res, n - i + 1);
-                while (l <= n and b >= k) {
-                    b ^= pref[l];
-                    res = min(res, n - l + 1);
-                    l++;
-                }
+            if (kk >= k) {
+                res = min(res, j - i);
             }
+            Remove(i);
         }
         return (res == n + 1 ? -1 : res);
     }
