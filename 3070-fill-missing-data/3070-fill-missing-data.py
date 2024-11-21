b2ebb5b3b4a54c323d43921d90e12f061a896138
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-11-21 20:55:38 +0530

    Time: 322 ms (96.83%), Space: 68.8 MB (73.26%) - LeetHub

--- 3070-fill-missing-data/3070-fill-missing-data.py
+++ 3070-fill-missing-data/3070-fill-missing-data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
-    products['quantity'] = products['quantity'].fillna(0)   
+    for i in range(len(products['name'])):
+        if pd.isnull(products['quantity'][i]):
+            products['quantity'][i] = 0
+            
     return products
     
