b77e578d46f5baafa5642ddaf7f4f477d2d7d31d
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2024-12-22 20:06:46 +0530

    Time: 159 ms (100%), Space: 101.6 MB (50%) - LeetHub

--- 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.cpp
+++ 3659-count-paths-with-the-given-xor-value/3659-count-paths-with-the-given-xor-value.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
 const int MOD = 1e9 + 7;
     int countPathsWithXorValue(vector<vector<int>>& grid, int k) { 
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<unordered_map<int, int>>> dp(m, vector<unordered_map<int, int>>(n));
-
-        function<int(int, int, int)> dfs = [&](int i, int j, int curr_xor) {
-            if (i == m - 1 && j == n - 1) {
-                return (curr_xor ^ grid[i][j]) == k ? 1 : 0;
+        int n = (int)grid.size();
+        int m = (int)grid[0].size();
+        vector dp(n, vector (m, vector<int> (16)));
+        dp[0][0][grid[0][0]] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int b = 0; b < 16; b++) {
+                    if (dp[i][j][b]) {
+                        if (i + 1 < n) {
+                            (dp[i + 1][j][b ^ grid[i + 1][j]] += dp[i][j][b]) %= MOD;
+                        }
+                        if (j + 1 < m) {
+                            (dp[i][j + 1][b ^ grid[i][j + 1]] += dp[i][j][b]) %= MOD;
+                        }
+                    }
+                }
             }
-
-            if (i >= m || j >= n) {
-                return 0;
-            }
-
-            if (dp[i][j].count(curr_xor)) {
-                return dp[i][j][curr_xor];
-            }
-
-            int res = 0;
-            res = (res + dfs(i, j + 1, curr_xor ^ grid[i][j])) % MOD;
-            res = (res + dfs(i + 1, j, curr_xor ^ grid[i][j])) % MOD;
-
-            return dp[i][j][curr_xor] = res;
-        };
-
-        return dfs(0, 0, 0);
+        }
+        return dp[n - 1][m - 1][k];
     }
 };
