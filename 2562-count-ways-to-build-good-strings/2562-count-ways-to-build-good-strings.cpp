f9a5fba7160bc483838f7e9f727cdb825f15c556
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2024-12-30 17:37:50 +0530

    Time: 127 ms (5.09%), Space: 69.8 MB (5.48%) - LeetHub

---
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef freakin23
+#include "library/debug.cpp"
+#else
+#define debug(...)
+#endif
+
+#define mod 1000000007
+#define ll long long
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<ll> ret(200005);
+        set<int> s ;
+        for(int i = 0; i < 10; i++){
+            s.insert(i);
+        }
+        ret[0] = 1;
+        for(int i = 0; i < 100005; i++){
+            ret[i + zero] %= mod;
+            ret[i + zero] += ret[i];
+            ret[i + one] %= mod;
+            ret[i + one] += ret[i];
+        }
+        ll res = 0;
+        for(int i = low; i <= high; i++){
+            res = (res + ret[i]) % mod;
+        }
+        return res;
+    }
+};
+
+#ifdef freakin23
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}
+#endif
