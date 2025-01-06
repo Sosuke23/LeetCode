f2d818d1d2832a2a5f35592944d0dea586c59186
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2025-01-06 13:17:33 +0530

    Time: 4 ms (27.76%), Space: 13.7 MB (10.37%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,27 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
-#ifdef freakin23
-#include "library/debug.cpp"
-#else
-#define debug(...)
-#endif
-
 class Solution {
 public:
-    int minChanges(string st) {
+    int minChanges(string s) {
         int res = 0;
-        for (int i = 0; i < (int)st.size(); i += 2) {
-            res += (st[i] != st[i + 1]);
+        for (int i = 0; i < (int)s.size(); i += 2) {
+            res += (s[i] != s[i + 1]);
         }
         return res;
     }
 };
-
-
-#ifdef freakin23
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    return 0;
-}
-#endif
