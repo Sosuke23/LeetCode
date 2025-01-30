4970f021eef9ed6f7cb31229a1fe27d6a86c8992
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2025-01-30 10:49:05 +0530

    Time: 155 ms (49.58%), Space: 37.9 MB (98.74%) - LeetHub

---
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> g(n + 5);
+        for (auto& e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        vector<int> dis(n + 5, n + 5);
+        queue<int> q;
+        vector<int> s(n + 5, 0);
+        for (int i = 1; i <= n; ++i) {
+            q.push(i);
+            fill(dis.begin(), dis.end(), n + 5);
+            dis[i] = 0;
+            int mx = 0, mi = i;
+            while (!q.empty()) {
+                int x = q.front();
+                q.pop();
+                for (int v : g[x])
+                    if (dis[v] == n + 5) {
+                        dis[v] = dis[x] + 1;
+                        mx = max(mx, dis[v]);
+                        mi = min(mi, v);
+                        q.push(v);
+                    } else if (dis[v] % 2 != (dis[x] + 1) % 2) {
+                        return -1;
+                    }
+            }
+            s[mi] = max(s[mi], mx + 1);
+        }
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            res += s[i];
+        }
+        return res;
+    }
+};
