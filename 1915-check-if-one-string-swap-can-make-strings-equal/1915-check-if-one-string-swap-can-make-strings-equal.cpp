1a06da07d4a646f8bb737743762610bab2d174d1
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2025-02-05 14:05:03 +0530

    Time: 2 ms (16.01%), Space: 8.5 MB (36.33%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<int>idx; //storing the index
-        for(int i=0;i<s1.size();i++){
-                //if the values are not equal then store its index.
-            if(s1[i]!=s2[i]){ 
+        vector<int> idx;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
                 idx.push_back(i);
             }
-            // if we have found more than 2 index where value does not match, 
-            // then break.
-            if (idx.size()>=2) break; 
+            if (idx.size() >= 2)
+                break;
         }
-        // if size==1, then we cannot swap, as swapping needs 2 values.
-        if(idx.size()==1)return false;
-        // if size of idx is zero then strings are already equal
-        else if(idx.size()==0) return true; 
+        if (idx.size() == 1) {
+            return false;
+        }
+        else if (idx.size() == 0) {
+            return true;
+        }
         else {
-            swap(s2[idx[0]],s2[idx[1]]); //swaping the indexes that we have found.
-            return s1==s2; //if after swap both the string are same.
+            swap(s2[idx[0]], s2[idx[1]]);
+            return s1 == s2;
         }
     }
 };
