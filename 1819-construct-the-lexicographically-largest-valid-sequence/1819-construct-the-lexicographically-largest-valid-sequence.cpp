996a34d174f4552130c81d6ce4ef6821828458bd
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2025-02-16 13:11:11 +0530

    Time: 3 ms (19.91%), Space: 8.9 MB (16.45%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> constructDistancedSequence(int n) {
-        vector<int> res(1 + (n - 1) * 2, -1);     
-        unordered_set<int> nums; 
-        for (int i = 1; i <= n; i++) nums.insert(i); 
+        vector<int> res(1 + (n - 1) * 2, -1);
+        unordered_set<int> nums;
+        for (int i = 1; i <= n; i++) {
+            nums.insert(i);
+        }
         dfs(0, n, res, nums);
         return res;
     }
     bool dfs(int i, int n, vector<int>& res, unordered_set<int>& nums) {
-        if (nums.size() == 0) return true; 
-        if (res[i] != -1) return dfs(i + 1, n, res, nums); 
+        if (nums.size() == 0) {
+            return true;
+        }
+        if (res[i] != -1) {
+            return dfs(i + 1, n, res, nums);
+        }
         for (int j = n; j >= 1; j--) {
             int dist = j == 1 ? 0 : j;
             if (nums.count(j) && (i + dist < res.size() && res[i + dist] == -1)) {
-                res[i] = res[i + dist] = j; 
-                nums.erase(j); 
-                if (dfs(i + 1, n, res, nums)) return true; 
+                res[i] = res[i + dist] = j;
+                nums.erase(j);
+                if (dfs(i + 1, n, res, nums)) {
+                    return true;
+                }
                 res[i] = res[i + dist] = -1;
                 nums.insert(j);
             }
@@ -23,4 +31,3 @@ public:
         return false;
     }
 };
-
