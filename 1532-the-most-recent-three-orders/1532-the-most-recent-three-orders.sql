3559b10aca9158ffa14df26bfcfb0ec83c43b6a0
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-03 11:18:39 +0530

    Time: 1023 ms (53.3%), Space: 0B (100%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,18 +1,8 @@
-# Write your MySQL query statement below
-SELECT
- c.name AS customer_name, o1.customer_id, o1.order_id, o1.order_date
-FROM
- orders o1
- LEFT JOIN
-      orders o2
-      ON o1.customer_id = o2.customer_id
-      AND o1.order_date < o2.order_date
- INNER JOIN
-       customers c
-       ON o1.customer_id = c.customer_id
-GROUP BY
- o1.customer_id, c.name, o1.order_id, o1.order_date
-HAVING
- COUNT(o2.order_date) <= 2
-ORDER BY
- c.name, o1.customer_id, o1.order_date desc
+select customer_name, customer_id, order_id, order_date 
+from (
+    select c.name as customer_name, o.customer_id, o.order_id, o.order_date, 
+       dense_rank() over (partition by o.customer_id order by o.order_date desc) as ranking
+    from Orders o join Customers c on o.customer_id=c.customer_id 
+) as temp
+where ranking <= 3
+order by customer_name asc, customer_id asc, order_date desc
