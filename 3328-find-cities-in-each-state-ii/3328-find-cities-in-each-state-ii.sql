9ff0fb8d798cc4c9a8e0d53797727129c2388b3b
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-03 11:22:09 +0530

    Time: 312 ms (45.28%), Space: 0B (100%) - LeetHub

--- 3328-find-cities-in-each-state-ii/3328-find-cities-in-each-state-ii.sql
+++ 3328-find-cities-in-each-state-ii/3328-find-cities-in-each-state-ii.sql
@@ -3,7 +3,7 @@ with cte as(
     select state,
            count(*) as matching_letter_count
     from cities
-    where substr(state,1,1)=substr(city,1,1)
+    where substr(state,1,1) = substr(city,1,1)
     group by state
 )
 select state,
@@ -12,5 +12,5 @@ select state,
 from cities left join cte using(state)
 where matching_letter_count is not null
 group by state
-having count(city)>=3
+having count(city) >= 3
 order by 3 desc,1;
