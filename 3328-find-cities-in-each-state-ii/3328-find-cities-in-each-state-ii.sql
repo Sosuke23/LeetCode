aff9acba9cebcf4dc7c18a005bd7de2b31453cdd
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-03 11:21:55 +0530

    Time: 308 ms (49.06%), Space: 0B (100%) - LeetHub

---
+++ 3328-find-cities-in-each-state-ii/3328-find-cities-in-each-state-ii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+with cte as(
+    select state,
+           count(*) as matching_letter_count
+    from cities
+    where substr(state,1,1)=substr(city,1,1)
+    group by state
+)
+select state,
+       group_concat(city order by city separator ', ') as cities,
+       matching_letter_count
+from cities left join cte using(state)
+where matching_letter_count is not null
+group by state
+having count(city)>=3
+order by 3 desc,1;
