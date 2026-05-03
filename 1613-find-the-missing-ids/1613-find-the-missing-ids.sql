31e5194b209bc939cc0fb05f8eefe7527c04590f
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-03 11:03:29 +0530

    Time: 434 ms (28.77%), Space: 0B (100%) - LeetHub

---
+++ 1613-find-the-missing-ids/1613-find-the-missing-ids.sql
@@ -0,0 +1,10 @@
+WITH RECURSIVE nums AS (
+    SELECT 1 AS value
+    UNION ALL
+    SELECT value + 1 AS value
+    FROM nums
+    WHERE nums.value < (select max(customer_id) from Customers)
+)
+
+select value  as ids from nums where value not in (select distinct customer_id from customers)
+order by ids
