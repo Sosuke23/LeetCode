c17acafc3738b228a6dd4165f6a683959becbe73
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-03 10:55:45 +0530

    Time: 481 ms (15.53%), Space: 0B (100%) - LeetHub

---
+++ 0614-second-degree-follower/0614-second-degree-follower.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select 
+    followee as follower, 
+    count(follower) as num
+from follow
+where followee in (select follower from follow)
+group by followee
+order by follower
