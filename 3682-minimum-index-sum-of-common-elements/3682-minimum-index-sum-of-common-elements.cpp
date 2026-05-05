08ca88ab0d955f2ebe910e57d7eb8d55688bfc19
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-06 00:28:02 +0530

    Time: 158 ms (60%), Space: 271.8 MB (100%) - LeetHub

--- 3682-minimum-index-sum-of-common-elements/3682-minimum-index-sum-of-common-elements.cpp
+++ 3682-minimum-index-sum-of-common-elements/3682-minimum-index-sum-of-common-elements.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     int minimumSum(vector<int>& nums1, vector<int>& nums2) {
         int n = (int)nums1.size();
-        std::unordered_map<int, std::vector<std::pair<int, int>>> pos;
-        for (int i = 0; i < n; i++) {
-            pos[nums1[i]].push_back({i, 1});
-        }
-        for (int i = 0; i < n; i++) {
-            pos[nums2[i]].push_back({i, 2});
+        std::unordered_map<int, int> umap;
+        for (int i = n - 1; i >= 0; i--) {
+            umap[nums1[i]] = i;
         }
 
         int res = INT_MAX;
-        for (auto &[_, val] : pos) {
-            std::sort(begin(val), end(val));
-            int prev = val[0].second;
-            int prev_i = val[0].first;
-            for (int i = 1; i < (int)val.size(); i++) {
-                if (prev != val[i].second) {
-                    res = std::min(res, prev_i + val[i].first);
-                    prev_i = val[i].first;
-                    prev = val[i].second;
-                }
+        
+        for (int i = 0; i < n; i++) {
+            if (umap.count(nums2[i])) {
+                res = std::min(res, umap[nums2[i]] + i);
             }
         }
+
         return (res == INT_MAX ? -1 : res);
     }
 };
