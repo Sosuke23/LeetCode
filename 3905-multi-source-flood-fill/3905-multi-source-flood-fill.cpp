6a4bd878c0b22b7998056fb81ee32ebb8ea11ae7
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-10 01:34:35 +0530

    Time: 555 ms (27.57%), Space: 342.6 MB (22.75%) - LeetHub

---
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
+        std::vector<std::vector<int>> res(n, std::vector<int> (m, 0));
+        std::sort(rbegin(sources), rend(sources), [&] (const auto &a, const auto &b) {
+            return a[2] > b[2];
+        });
+
+        for (auto x : sources) {
+            res[x[0]][x[1]] = x[2];
+        }
+
+        if (sources.front()[2] == sources.back()[2]) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    res[i][j] = sources.back()[2];
+                }
+            }
+            return res;
+        }
+                
+        const int dx[4] = {-1, 1, 0, 0};
+        const int dy[4] = {0, 0, -1, 1};
+
+        while (!sources.empty()) {
+            std::vector<std::vector<int>> temp_sources;
+            while (!sources.empty()) {
+                int row, col, colour;
+                row = sources.back()[0], col = sources.back()[1], colour = sources.back()[2]; 
+                sources.pop_back();
+                
+                for (int i = 0; i < 4; i++) {
+                    int new_row = row + dx[i];
+                    int new_col = col + dy[i];
+
+                    if (new_row >= 0 and new_row < n and new_col >= 0 and new_col < m and res[new_row][new_col] == 0) {
+                        temp_sources.push_back({new_row, new_col, colour});
+                        // std::cout << row << " " << col << ": " << new_row << " " << new_col << ": " << colour << '\n';
+                        res[new_row][new_col] = colour;
+                    }
+                }
+            }
+            sources = temp_sources;
+            reverse(begin(sources), end(sources));
+        }
+        return res;
+    }
+};
