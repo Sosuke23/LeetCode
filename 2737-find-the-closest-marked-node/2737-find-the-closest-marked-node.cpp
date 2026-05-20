907ad6232f9393f310820dcd668f928908b1ea9e
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-20 22:09:44 +0530

    Time: 61 ms (5%), Space: 85.9 MB (35%) - LeetHub

---
+++ 2737-find-the-closest-marked-node/2737-find-the-closest-marked-node.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int minimumDistance(int n, vector<vector<int>>& edges, int from, vector<int>& marked) {
+        std::vector<std::pair<int, int>> adj[n];
+        std::vector<bool> used(n, 0);
+        std::vector<long long> dis(n, INT_MAX);
+        int res = INT_MAX;
+
+        for (std::vector<int> e : edges) {
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+
+        std::priority_queue<pair<int, int>> PQ;
+
+        dis[from] = 0; 
+        PQ.push({0, from});
+        while (!PQ.empty()) {
+            int a = PQ.top().second;
+            PQ.pop();
+
+            if (used[a]) {
+                continue;
+            }
+            used[a] = true;
+            for (auto u : adj[a]) {
+                int b = u.first;
+                int w = u.second;
+                if (dis[a] + w < dis[b]) {
+                    dis[b] = dis[a] + w;
+                    PQ.push({-dis[b], b});
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            cout << dis[i] << " \n"[i == n - 1];
+        }
+
+        for (int node : marked) {
+            if (dis[node] <= INT_MAX) {
+                res = std::min(res, (int)dis[node]);
+            }
+        }
+
+        return (res >= INT_MAX ? -1 : res);
+    }
+};
