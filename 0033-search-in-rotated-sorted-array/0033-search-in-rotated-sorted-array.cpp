18eb221bce7d97fe135a24080ac5c188a963d753
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-05-22 21:48:15 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.43%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return (find(begin(nums), end(nums), target) == nums.end() ? -1 : find(begin(nums), end(nums), target) - begin(nums));
+    }
+};
