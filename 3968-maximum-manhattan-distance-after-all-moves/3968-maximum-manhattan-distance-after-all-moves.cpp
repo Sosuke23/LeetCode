f3b1ef59e9e5e8509403378d5251a4c5be7ad9fd
Rameez Parwez <79394137+Sosuke23@users.noreply.github.com>
2026-06-26 20:59:26 +0530

    Time: 27 ms (10.26%), Space: 22 MB (90.4%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxDistance(string moves) {
+        int x = 0, y = 0;
+        int n = (int)moves.size();
+
+        for (int i = 0; i < n; i++) {
+            char m = moves[i];
+            if (m == 'D') {
+                y -= 1;
+            } else if (m == 'L') {
+                x -= 1;
+            } else if (m == 'U') {
+                y += 1;
+            } else if (m == 'R') {
+                x += 1;
+            }
+        }
+
+        int c = std::count(begin(moves), end(moves), '_');
+        int res = std::abs(x) + std::abs(y) + c;
+        return res;
+    }
+};
